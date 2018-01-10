2fa28b7c07d0b9db9db294b6656c8b1da339cbe8
mineru <rmstjr1030@naver.com>
2018-01-10 16:55:39 +0900

    Add files via upload

---
+++ app/src/main/java/com/mineru/hops/Function/AddGroup/Add_Group.java
@@ -0,0 +1,20 @@
+package com.mineru.hops.Function.AddGroup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mineru.hops.R;
+
+/**
+ * Created by mineru on 2018-01-10.
+ */
+
+public class Add_Group extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_group);
+
+    }
+
+}
