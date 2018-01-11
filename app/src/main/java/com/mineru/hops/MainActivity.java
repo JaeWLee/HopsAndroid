ff0e8be782a42fffc876fd7b64e4fedc6afe49e7
mineru <rmstjr1030@naver.com>
2018-01-11 19:01:43 +0900

    2018.1.11 modified in office

--- app/src/main/java/com/mineru/hops/MainActivity.java
+++ app/src/main/java/com/mineru/hops/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,7 +17,6 @@ import com.mineru.hops.Fragment.Home;
 import com.mineru.hops.Fragment.Latter;
 import com.mineru.hops.Fragment.List;
 import com.mineru.hops.UserManage.LoginActivity;
-import com.mineru.hops.UserManage.SignOutActivity;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -113,28 +111,6 @@ public class MainActivity extends AppCompatActivity {
                 // ...
             }
         };
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int curId = item.getItemId();
-        switch(curId){
-            case R.id.main_menu_setting:
-                Intent intent1 = new Intent(this,MenuFragment.class);
-                startActivity(intent1);
-                break;
-            case R.id.main_menu_signout:
-                Intent intent2 = new Intent(this,SignOutActivity.class);
-                startActivity(intent2);
-                break;
-            default:
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
     @Override
     public void onStart() {
